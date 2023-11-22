104e533aab46534bab1ee29014804638f36c8cd0
Pepe G. Krumminga <pepe.krumminga@study.hs-duesseldorf.de>
2023-11-22 15:00:06 +0100

    Set up the base repository using cookiecutter

---
+++ cellular_automata/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about cellular_automata"""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Pepe Krumminga"
+__email__ = "krumminga.pepe@gmail.com"
+__version__ = "0.1.0"
