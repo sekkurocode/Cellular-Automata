a6a520c4a5534175dd8e8a81db2b8dbc330c6b60
Pepe G. Krumminga <pepe.krumminga@study.hs-duesseldorf.de>
2023-12-06 16:11:01 +0100

    Added .py file and made skeleton code

---
+++ cellular_automata/game_of_life.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Player(self):
+    pass
+
+
+class Game(self):
+    def __init__(self):
+        self.rules 
+        pass
+    
+    def start(self):
+        board = Board(5, 5)
+        board.create()
+        
+    pass
+
+
+class Board(self, rows, cols):
+    def create():
+        board = np.zeros(self.rows, self.cols)
+        pass
+    
+    def next_generation():
+        pass
+    
