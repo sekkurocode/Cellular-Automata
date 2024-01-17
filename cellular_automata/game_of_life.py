35ae76b90e1956ac8112c1f2d665c8bc7199c308
Fabrice Eberle <fabrice.eberle@gmail.com>
2024-01-17 15:21:29 +0100

    issue11 working gameloop

--- cellular_automata/game_of_life.py
+++ cellular_automata/game_of_life.py
@@ -1,52 +1,50 @@
 import numpy as np
 
-class Player(self):
+class Player():
     pass
 
-
-class Game(self,numberOfColumns=10,numberOfRows=10):
-    def __init__(self):
-        self.rules 
-        self.numcol = numberOfColumns
-        self.numrow = numberOfRows
-        self.numNeighbors = 0
-        pass
-    
-    def start(self):
-        
-# Randomly fill the board with 0's and 1's
-# for rows in range(numberOfRows):
-#   for columns in range(numberOfColumns):
-#     board[rows][columns] = random.randint(0, 1)
-
-        board = np.random.choice([0, 1], size=(10, 10), p=[0.7, 1-0.7]) 
-        for rows in range(self.numrow):
-            for columns in range(self.numcol):
-                # checks all neighboring cells
-                if board[rows,columns]:
-                    numNeighbors = np.sum(board[rows-1:rows+2,columns-1:columns+2])-1
-                else:
-                    numNeighbors = np.sum(board[rows-1:rows+2,columns-1:columns+2])
-                
-                    # Update the status for the next generation, following the rules 
-                if numNeighbors >= 4:
-                    board[rows,columns] = 0
-                if numNeighbors == 3:
-                    board[rows,columns] = 1
-                elif numNeighbors <= 2:
-                    board[rows,columns] = 0
-                # Reset the number of neighbors for checking the next array position
-                    
-                numNeighbors = 0
-
-        print(board)
- 
-
-class Board(self, rows, cols):
+class Board():
     def create():
         board = np.zeros(self.rows, self.cols)
         pass
     
     def next_generation():
         pass
+
+
+class Game():
+    def __init__(self):
+        self.numcol = 10
+        self.numrow = 10
+        self.numNeighbors = 0
+        self.numiter = 10
+        self.board = np.random.choice([0, 1], size=(self.numrow, self.numcol), p=[0.7, 0.3])
+        self.temp_board = np.zeros([self.numrow, self.numcol]).astype(int)
     
+    def start(self):
+        print(self.board)
+        for _ in range(self.numiter):
+            for rows in range(self.numrow):
+                for columns in range(self.numcol):
+                    if self.board[rows,columns]:
+                        self.numNeighbors = np.sum(self.board[rows-1:rows+2,columns-1:columns+2])-1
+                    else:
+                        self.numNeighbors = np.sum(self.board[rows-1:rows+2,columns-1:columns+2])
+                        
+                        # Update the status for the next generation
+                    if self.numNeighbors >= 4:
+                        self.temp_board[rows,columns] = 0
+                    if self.numNeighbors == 3:
+                        self.temp_board[rows,columns] = 1
+                    elif self.numNeighbors <= 2:
+                        self.temp_board[rows,columns] = 0
+                        # Reset the number of neighbors for checking the next array position
+                        
+                    self.numNeighbors = 0
+            self.board = self.temp_board
+            self.temp_board = np.zeros([self.numrow, self.numcol]).astype(int)
+            print(self.board)
+      
+new_game=Game()
+
+new_game.start()
