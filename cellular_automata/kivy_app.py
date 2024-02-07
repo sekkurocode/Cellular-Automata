f45b4104f9868c1dc9c4258bbe5e69249b0f100b
Pepe Krumminga <krumminga.pepe@gmail.com>
2024-02-07 16:54:19 +0100

    Login Screen Experiment

--- cellular_automata/kivy_app.py
+++ cellular_automata/kivy_app.py
@@ -1,14 +1,29 @@
 import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
+kivy.require('2.2.1') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
+button = Button(text='Start Game', font_size=14)
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Welcome to the world of Cellular Automata!')
+        return MyGrid()
 
 
 if __name__ == '__main__':
